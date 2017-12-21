e006b08d164369d30dcf5f77c8ac7e3dd8fafbca
Mohamamd Lotfi <admin@lotfi.xyz>
2017-12-21 13:42:24 +0330

    Update.

--- routes/helper/web/Hamahang/helper/HamahangUsersRoutes/UserList.php
+++ routes/helper/web/Hamahang/helper/HamahangUsersRoutes/UserList.php
@@ -1,9 +1,9 @@
 <?php
 
-Route::group(['prefix' => 'user_list', 'middleware' => ['dynamic_permission:ugc.desktop.hamahang.user_list']], function ($username)
+Route::group(['prefix' => 'user_list', 'middleware' => ['role:administrator']], function ($username)
 {
     //Route::get('/', ['as' => 'ugc.desktop.hamahang.user_list.index', 'uses' => 'UserController@index', 'middleware' => ['dynamic_permission:ugc.desktop.hamahang.user_list.index']]);
-    Route::get('/', ['as' => 'ugc.desktop.hamahang.user_list.index', 'uses' => 'UserController@index', 'middleware' => ['role:registerd']]);
-    Route::get('edit',['as' => 'ugc.desktop.hamahang.user_list.edit', 'uses' => 'UserController@user_list_edit', 'middleware' => ['dynamic_permission:ugc.desktop.hamahang.user_list.edit']]);
+    Route::get('/', ['as' => 'ugc.desktop.hamahang.user_list.index', 'uses' => 'UserController@index', /*'middleware' => ['role:registerd']*/]);
+    Route::get('edit',['as' => 'ugc.desktop.hamahang.user_list.edit', 'uses' => 'UserController@user_list_edit', /*'middleware' => ['dynamic_permission:ugc.desktop.hamahang.user_list.edit']*/]);
     /* ??? */Route::get('add', 'UserController@user_list_add');
 });
