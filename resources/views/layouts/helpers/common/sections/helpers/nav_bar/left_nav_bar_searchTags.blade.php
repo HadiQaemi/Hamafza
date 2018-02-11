f5f12eaf55fc74457f34d34eac065904459fd8ec
Mohammad Lotfi <admin@lotfi.xyz>
2018-02-11 08:54:49 +0330

    Update...

--- resources/views/layouts/helpers/common/sections/helpers/nav_bar/left_nav_bar_searchTags.blade.php
+++ resources/views/layouts/helpers/common/sections/helpers/nav_bar/left_nav_bar_searchTags.blade.php
@@ -32,14 +32,17 @@
                     }
                     case 'enquiry_pages':
                     {
-                        if (20 == $keyword_item->subject->kind)
+                        if ($keyword_item->subject)
                         {
-                            $r .=
-                            '
-                            <li id="keyword_' . $keyword_item->id . '" style="list-style: inside none square; margin-bottom: 5px;">
-                                <a rel="canonical" href="' . url("{$keyword_item->sid}0/enquiry/$keyword_item->id") . '" target="_blank">' . $keyword_item->title . '</a>
-                            </li>
-                            ';
+                            if (20 == $keyword_item->subject->kind)
+                            {
+                                $r .=
+                                '
+                                <li id="keyword_' . $keyword_item->id . '" style="list-style: inside none square; margin-bottom: 5px;">
+                                    <a rel="canonical" href="' . url("{$keyword_item->sid}0/enquiry/$keyword_item->id") . '" target="_blank">' . $keyword_item->title . '</a>
+                                </li>
+                                ';
+                            }
                         }
                         break;
                     }
