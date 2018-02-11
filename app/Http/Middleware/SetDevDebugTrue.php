d80047c3e3d222a67e4cd440a899925209b05a37
Mohammad Lotfi <admin@lotfi.xyz>
2018-02-11 08:21:51 +0330

    Update SetDevDebugTrue

--- app/Http/Middleware/SetDevDebugTrue.php
+++ app/Http/Middleware/SetDevDebugTrue.php
@@ -1,9 +1,11 @@
 <?php namespace App\Http\Middleware;
 
+use App\Classes\SmartdetectClass;
 use Auth;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\Foundation\Application;
+use Schema;
 
 class SetDevDebugTrue
 {
@@ -17,7 +19,18 @@ class SetDevDebugTrue
 
     public function handle(Request $request, Closure $next)
     {
-        if (in_array($request->getClientIp(), ['89.165.122.115', '188.34.116.207', '127.0.0.1']))
+        $turn_on = false;
+        if (Schema::hasTable(\App\Smartdetect::schema_table))
+        {
+            $smartdetect = new SmartdetectClass();
+            if ($smartdetect->results['ip'])
+            {
+                $turn_on = true;
+            }
+        }
+
+        //if (in_array($request->getClientIp(), ['89.165.122.115', '188.34.116.207', '127.0.0.1']))
+        if ($turn_on)
         {
             config(['app.debug' => true]);
             \Debugbar::disable();
