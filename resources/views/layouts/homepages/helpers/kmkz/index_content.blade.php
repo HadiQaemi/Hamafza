b731098785f48d783f3010a76668b272fc1c9695
Mohammad Lotfi <admin@lotfi.xyz>
2018-02-15 05:28:58 +0330

    تیتر توضیحات درگاه های صفحه اول توضیحات درج شده در صفحه درگاه ها در لینک صفحه اول آنها دیده شوند به جز درگاه شبکه اجتماعی

--- resources/views/layouts/homepages/helpers/kmkz/index_content.blade.php
+++ resources/views/layouts/homepages/helpers/kmkz/index_content.blade.php
@@ -19,29 +19,36 @@
                         <li style="width: 25%; text-align: center;margin:0px"><a data-toggle="pill" href="#menu2">شبکه اجتماعی</a></li>
                         <li style="width: 25%; text-align: center;margin:0px"><a data-toggle="pill" href="#menu3">سازمان‌ها</a></li>
                     </ul>
-                    <style>
-
-                    </style>
                     <div class="tab-content">
                         <div id="home" class="tab-pane fade in active menus">
                             <div class="row">
                                 <div class="col-sm-3">
-                                    <a href="{{ url('/444500') }}"><img src="{{ url(App\Models\hamafza\Pages::find(444500)->subject->defimageurl) }}" /></a>
+                                    @php
+                                        $pid = '444500';
+                                        $page = App\Models\hamafza\Pages::find($pid);
+                                        $page_subject = $page->subject;
+                                    @endphp
+                                    <a href="{{ url("/$pid") }}"><img src="{{ url($page_subject->defimageurl) }}" /></a>
                                 </div>
                                 <div class="col-sm-9 tab-des">
-                                    <h3>فرآیند بودجه: تنظیم، تصویب، اجرا و کنترل</h3>
-                                    <p>متن نوشته شده جهت تست</p>
+                                    <h3>آثار علمی و پژوهشی</h3>
+                                    <p>{!! $page_subject->ShortFirstPageDescription !!}</p>
                                 </div>
                             </div>
                         </div>
                         <div id="menu1" class="tab-pane fade menus">
                             <div class="row">
                                 <div class="col-sm-3">
-                                    <a href="{{ url('/444490') }}"><img src="{{ url(App\Models\hamafza\Pages::find(444490)->subject->defimageurl) }}" /></a>
+                                    @php
+                                        $pid = '444490';
+                                        $page = App\Models\hamafza\Pages::find($pid);
+                                        $page_subject = $page->subject;
+                                    @endphp
+                                    <a href="{{ url("/$pid") }}"><img src="{{ url($page_subject->defimageurl) }}" /></a>
                                 </div>
                                 <div class="col-sm-9 tab-des">
-                                    <h3>فرآیند بودجه: تنظیم، تصویب، اجرا و کنترل</h3>
-                                    <p>متن نوشته شده جهت تست</p>
+                                    <h3>پایگاه‌های علمی داخلی و خارجی</h3>
+                                    <p>{!! $page_subject->ShortFirstPageDescription !!}</p>
                                 </div>
                             </div>
                         </div>
@@ -51,19 +58,24 @@
                                     <a href="{{ url('/4') }}"><img src="{{ url('theme/kmkz/img/4.png') }}" /></a>
                                 </div>
                                 <div class="col-sm-9 tab-des">
-                                    <h3>فرآیند بودجه: تنظیم، تصویب، اجرا و کنترل</h3>
-                                    <p>متن نوشته شده جهت تست</p>
+                                    <h3>شبکه های اجتماعی</h3>
+                                    <p>&nbsp;</p>
                                 </div>
                             </div>
                         </div>
                         <div id="menu3" class="tab-pane fade menus">
                             <div class="row">
                                 <div class="col-sm-3">
-                                    <a href="{{ url('/444480') }}"><img src="{{ url(App\Models\hamafza\Pages::find(444480)->subject->defimageurl) }}" /></a>
+                                    @php
+                                        $pid = '444480';
+                                        $page = App\Models\hamafza\Pages::find($pid);
+                                        $page_subject = $page->subject;
+                                    @endphp
+                                    <a href="{{ url("/$pid") }}"><img src="{{ url($page_subject->defimageurl) }}" /></a>
                                 </div>
                                 <div class="col-sm-9 tab-des">
-                                    <h3>فرآیند بودجه: تنظیم، تصویب، اجرا و کنترل</h3>
-                                    <p>متن نوشته شده جهت تست</p>
+                                    <h3>سازمان‌های استان خوزستان</h3>
+                                    <p>{!! $page_subject->ShortFirstPageDescription !!}</p>
                                 </div>
                             </div>
                         </div>
@@ -75,12 +87,17 @@
                     <h2>اخبار و اطلاعیه</h2>
                     <div class="row">
                         <div class="col-sm-4">
-                            <a href="{{ url('/444510') }}"><img class="slide_image" src="{{ url(App\Models\hamafza\Pages::find(444510)->subject->defimageurl) }}" /></a>
+                            @php
+                                $pid = '444510';
+                                $page = App\Models\hamafza\Pages::find($pid);
+                                $page_subject = $page->subject;
+                            @endphp
+                            <a href="{{ url("/$pid") }}"><img class="slide_image" src="{{ url($page_subject->defimageurl) }}" /></a>
                             <p class="change_slider"><a href="#"><</a>همه<a href="#">></a></p>
                         </div>
                         <div class="col-sm-8 tab-des">
-                            <h3>آغاز بهره برداری از پروژه مشترک صدور نفت</h3>
-                            <p>متن نوشته شده جهت تست میباشد.</p>
+                            <h3>رویدادهای مهم</h3>
+                            <p>{!! $page_subject->ShortFirstPageDescription !!}</p>
                         </div>
                     </div>
                 </div>
