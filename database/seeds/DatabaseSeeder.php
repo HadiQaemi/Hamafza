33c80a717ac7646ddf4feae20956a980e6db2c00
Mohamamd Lotfi <admin@lotfi.xyz>
2018-03-03 09:33:23 +0330

    اصلاحات  برای نسخه قابل نصب

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+        DB::table('user')->truncate();
+        DB::table('user')->insert
+        ([
+            'Uname' => 'hamafza',
+            'password' => '$2y$10$TWc9wBHZPeQkjscyctwlt.n.kNncye9a6EC8qasEd2MINw21rH2Ta',
+            'Email' => 'info@hamafza.ir',
+            'Name' => '-',
+            'Family' => '-',
+            //'created_at' => date('Y-m-d h:i:s'),
+        ]);
+
+        DB::table('smartdetect')->truncate();
+        DB::table('smartdetect')->insert
+        ([
+            'content' => '89.165.122.115',
+            'content_type' => 'ip',
+        ]);
+        DB::table('smartdetect')->insert
+        ([
+            'content' => '5.202.232.229',
+            'content_type' => 'ip',
+        ]);
+        DB::table('smartdetect')->insert
+        ([
+            'content' => '5.202.233.153',
+            'content_type' => 'ip',
+        ]);
+        DB::table('smartdetect')->insert
+        ([
+            'content' => 'dev',
+            'content_type' => 'request_any',
+        ]);
+
     }
 }
