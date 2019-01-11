589076201f24f7f3f5139688a8f8d50de7f8a44a
hadi <hadi.qaemi@gmail.com>
2019-01-11 12:27:49 +0330

    undo help

--- app/HamafzaServiceClasses/PageClass.php
+++ app/HamafzaServiceClasses/PageClass.php
@@ -19,13 +19,110 @@ class PageClass extends PageClassExtend1
         $open = '\{\{';
         $close = '\}\}';
         $matches = [];
+        $pattern = "$open$term\|(.+)(\|[0-9a-z]*)$close(.+)$open$term$close";
+        $preg_match = preg_match_all("/$pattern/is", $html, $matches, PREG_SET_ORDER);
+        return $preg_match ? $matches : false;
+    }
+
+    public static function helper_viewer($html)
+    {
+        $term = self::help_tag_term;
+        $open = '{{';
+        $close = '}}';
+        $explode = explode("$open$term$close", $html);
+        $r = [];
+        foreach ($explode as $v)
+        {
+            $matches = self::helper_finder("$v$open$term$close");
+            if (false == $matches)
+            {
+                $r[] = $v;
+            } else
+            {
+                foreach ($matches as $match)
+                {
+                    $r[] = str_replace($match[0], $match[3], "$v$open$term$close");
+                }
+            }
+        }
+        return implode('', $r);
+    }
+
+    public static function helper_maker($html, $page_id)
+    {
+        $term = self::help_tag_term;
+        $open = '{{';
+        $close = '}}';
+        $explode = explode("$open$term$close", $html);
+        foreach ($explode as $v)
+        {
+            $is_new = false;
+            $matches = self::helper_finder("$v$open$term$close");
+            if (false == $matches)
+            {
+                $r[] = $v;
+            } else
+                foreach ($matches as $match_k => $match)
+                {
+                    $title = trim($match[1]);
+                    $code = trim($match[2]);
+                    $content = trim($match[3]);
+                    $created_by = auth()->id();
+                    if ('|0' == $code)
+                    {
+                        $is_new = true;
+                    } else
+                    {
+                        $explode = explode('h', str_replace('|', null, $code));
+                        if (2 == count($explode))
+                        {
+                            if (hash('crc32', $explode[0]) == $explode[1])
+                            {
+                                $is_new = false;
+                                $id = $explode[0];
+                            } else
+                            {
+                                $is_new = true;
+                            }
+                        }
+                    }
+                    $help_data_array = ['title' => $title, 'created_by' => $created_by, ];
+                    $help = Help::where('title', $help_data_array['title']);
+                    $help_id = $help->count() ? $help->first()->id : Help::create($help_data_array)->id;
+                    $help_block_data_array = ['help_id' => $help_id, 'page_id' => $page_id, 'content' => $content, 'created_by' => auth()->id(), ];
+                    if ($is_new)
+                    {
+                        $term = self::help_tag_term;
+                        $help_block = HelpBlock::create($help_block_data_array);
+                        $crc32 = hash('crc32', $help_block->id);
+                        $r[] = str_replace($matches[$match_k][0], $open . $term . '|' . $title . '|' . $help_block->id . 'h' . $crc32 . $close . $content . $open . $term . $close, "$v$open$term$close");
+                    } else
+                    {
+                        $help_block = HelpBlock::find($id);
+                        $help_block->update($help_block_data_array);
+                        $r[] = "$v$open$term$close";
+                    }
+                }
+            {
+            }
+        }
+        return implode('', $r);
+    }
+
+
+    public static function helper_finder_new($html)
+    {
+        $term = self::help_tag_term;
+        $open = '\{\{';
+        $close = '\}\}';
+        $matches = [];
 //        $pattern = "$open$term\|(.+)(\|[0-9a-z]*)$close(.+)$open$term$close";
         $pattern = "$open$term(.+?)$close(.+)$open$term$close";
         $preg_match = preg_match_all("/$pattern/is", $html, $matches, PREG_SET_ORDER);
         return $preg_match ? $matches : false;
     }
 
-    public static function helper_viewer($html)
+    public static function helper_viewer_new($html)
     {
         $term = self::help_tag_term;
         $open = '{{';
@@ -49,7 +146,7 @@ class PageClass extends PageClassExtend1
         return implode('', $r);
     }
 
-    public static function helper_maker($html, $page_id)
+    public static function helper_maker_new($html, $page_id)
     {
 
         $term = self::help_tag_term;
@@ -94,5 +191,6 @@ class PageClass extends PageClassExtend1
         }
         return implode('', $r);
     }
+
 
 }
--- app/Http/Controllers/Hamahang/HelpController.php
+++ app/Http/Controllers/Hamahang/HelpController.php
@@ -36,6 +36,42 @@ class HelpController extends Controller
         return $r;
     }
 
+    public function help_content()
+    {
+        //$help = Help::query();
+        $help = Help::whereHas('HelpBlocks');
+        $r = Datatables::eloquent($help)
+            ->editColumn('id', function ($q) { return enCode($q->id); })
+            ->addColumn('blocks_count', function ($q) { return $q->blocks_count; })
+            ->addColumn('usages', function ($q)
+            {
+                $r = null;
+                $PRE_r = [];
+                $items = $q->usages;
+                foreach ($items as $item)
+                {
+                    $PRE_r[] = '<a href="' . url($item->id) . '" target="_blank">' . $item->subject->title . '</a>';
+                }
+                $r = implode(', ', $PRE_r);
+                return $r ? $r : '-';
+            })
+            ->addColumn('see_also', function ($q)
+            {
+                $r = null;
+                $PRE_r = [];
+                $items = $q->SeeAlsos;
+                foreach ($items as $item)
+                {
+                    $PRE_r[] = $item->title;
+                }
+                $r = '<a class="jsPanels" href="' . route('modals.help.seealso') . '?id=' . enCode($q->id) . '">' . (empty($PRE_r) ? 'بدون پیوند' : implode(', ', $PRE_r)) . '<a>';
+                return $r;
+            })
+            ->rawColumns(['usages', 'see_also'])
+            ->make(true);
+        return $r;
+    }
+
     public function AddHelpPermission()
     {
         DB::table('hamahang_help_relations')->insert
